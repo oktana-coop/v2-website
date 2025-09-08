c6c702ffaaa402eb3a1ec8e9bcc79600b6c6eefe
Aris Goudouras <aris.gk@gmail.com>
2025-09-08 11:30:31 +0300

    chore: add Tailwind CSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
   site: "https://v2editor.com",
   outDir: "./docs",
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
